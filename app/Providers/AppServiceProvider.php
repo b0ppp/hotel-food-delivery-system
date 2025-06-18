728ffc895ae0ad313caa916ce407a54d5100d726
b0ppp <kalladararaja84@gmail.com>
2025-06-18 21:48:25 +0700

    Modifikasi AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') === 'production') {
-            URL::forceScheme('httpss');
+            URL::forceScheme('https');
         }
     }
 
